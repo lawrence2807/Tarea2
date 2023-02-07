d393a77e81fd2920665d0272488231fefbf5a2ea
lawrence2807 <lawrencect28@gmail.com>
2023-02-06 23:42:24 -0600

    Creación Tarea 2 configuración de trabajo

---
+++ src/main/java/Tarea2com/example/Tarea2Application.java
@@ -0,0 +1,13 @@
+package Tarea2com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea2Application.class, args);
+	}
+
+}
